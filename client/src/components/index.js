fc509646e0da5d648dd1af843a823b12105e6506
Suriyaa <suriyaaap13@gmail.com>
2023-03-10 23:47:01 +0530

    Setup the directory to start coding

---
+++ client/src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Navbar } from './Navbar';
+export { default as Footer } from './Footer';
+export { default as Services } from './Services';
+export { default as Welcome } from './Welcome';
+export { default as Transactions } from './Transactions';
